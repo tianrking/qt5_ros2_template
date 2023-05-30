39b682159368393ea7c80a825e24f03ca8aaccef
w0x7ce <tian.r.king@gmail.com>
2023-05-30 20:08:41 +0800

    Add: ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,37 @@
 #include <rclcpp/rclcpp.hpp>
 #include <QApplication>
+#include <QUiLoader>
+#include <QFile>
+#include <QWidget>
 #include <QPushButton>
+//#include <ament_index_cpp/get_package_share_directory.hpp>
 #include <std_msgs/msg/string.hpp>
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   QApplication app(argc, argv);
 
-  QPushButton button("Hello, ROS2 with Qt5!");
+  // Load the UI file using QUiLoader
+  QUiLoader loader;
+  QFile file("../ui/untitled.ui");
+  file.open(QFile::ReadOnly);
+  QWidget *window = loader.load(&file);
+  file.close();
 
   auto node = rclcpp::Node::make_shared("my_node");
   auto publisher = node->create_publisher<std_msgs::msg::String>("topic", 10);
 
-  QObject::connect(&button, &QPushButton::clicked, [&]() {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, ROS2!";
-      publisher->publish(message);
-  });
+  // Assuming there's a QPushButton in your UI file with the objectName "myButton"
+  QPushButton *button = window->findChild<QPushButton*>("pushButton");
+  if (button != nullptr) {
+    QObject::connect(button, &QPushButton::clicked, [&]() {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello, ROS2!";
+        publisher->publish(message);
+    });
+  }
 
-  button.show();
+  window->show();
 
   std::thread([&]() {
       rclcpp::spin(node);
